7692d42bf579b8b1e2a8521751b942f5d5602018
jon-smith <jonathansmith24601@outlook.com>
2018-01-07 16:05:40 +0000

    Thread safe lazy evaluator and unit test

--- Obelisk/ObeliskCore/LazyEval.hpp
+++ Obelisk/ObeliskCore/LazyEval.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <functional>
+#include <mutex>
 
 #include "Optional.h"
 
@@ -10,6 +11,8 @@ namespace obelisk
 	class LazyEval
 	{
 	public:
+
+		LazyEval() = delete;
 
 		template <typename Fn>
 		LazyEval(Fn &&fn) : fn(fn)
@@ -25,6 +28,33 @@ namespace obelisk
 		}
 
 	private:
+		mutable Optional<ReturnT> value;
+		std::function<ReturnT(void)> fn;
+	};
+
+	template <typename ReturnT>
+	class LazyEvalThreadSafe
+	{
+	public:
+
+		LazyEvalThreadSafe() = delete;
+
+		template <typename Fn>
+		LazyEvalThreadSafe(Fn &&fn) : fn(fn)
+		{
+
+		}
+
+		ReturnT get() const
+		{
+			std::lock_guard<std::mutex> lk(m);
+			if (!value.hasValue())
+				value.set(fn());
+			return value.value();
+		}
+
+	private:
+		mutable std::mutex m;
 		mutable Optional<ReturnT> value;
 		std::function<ReturnT(void)> fn;
 	};
--- Obelisk/ObeliskTest/LazyEvalTest.cpp
+++ Obelisk/ObeliskTest/LazyEvalTest.cpp
@@ -2,6 +2,9 @@
 #include "CppUnitTest.h"
 #include "LazyEval.hpp"
 
+#include <future>
+#include <vector>
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace obelisk;
 
@@ -34,6 +37,54 @@ namespace ObeliskTest
 				// Confirm eval func is only called once
 				Assert::IsTrue(evalCount == 1);
 			}
+		}
+
+		TEST_METHOD(TestLazyEvaluation_MultiThread)
+		{
+			// Create a struct with a few members
+			// to make it more likely to fail if there's a thread safety problem
+			struct StructWithAFewVariables
+			{
+				StructWithAFewVariables(double d, int i, std::string str) : d(d), i(i), str(str) {};
+				double d;
+				int i;
+				std::string str;
+			};
+			const StructWithAFewVariables result(5.5, 1, "banana");
+
+			unsigned evalCount = 0;
+			auto const evalFn = [&evalCount, result]() {
+				evalCount++;
+				return result;
+			};
+
+			// Construct
+			LazyEvalThreadSafe<StructWithAFewVariables> lazyCalc(evalFn);
+			// Check the function hasn't yet been called
+			Assert::IsTrue(evalCount == 0, L"EvalFn called early");
+
+			// Spawn loads of threads to check safety
+			// Make sure the result doesnt change
+			// and the eval func only gets called once
+			// This isn't a perfect test of thread safety - but at least it tries...
+
+			std::vector<std::future<void>> threads;
+
+			for (int i = 0; i < 256; ++i)
+			{
+				threads.push_back(std::async(std::launch::async, [&lazyCalc, &evalCount, &result](){
+					// Confirm that we get the right result
+					const auto lazyEvalResult = lazyCalc.get();
+					Assert::IsTrue(result.d == lazyEvalResult.d);
+					Assert::IsTrue(result.i == lazyEvalResult.i);
+					Assert::IsTrue(result.str == lazyEvalResult.str);
+					// Confirm eval func is only called once
+					Assert::IsTrue(evalCount == 1);
+				}));
+			}
+
+			for (const auto &t : threads)
+				t.wait();
 		}
 	};
 }
