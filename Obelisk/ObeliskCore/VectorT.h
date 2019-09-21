fb71ec582ad5e04894000f6b6891a2f6c6fe11e4
jon-s <jonathansmith24601@outlook.com>
2019-09-21 19:04:31 +0100

    convenience aliases for vector2

--- Obelisk/ObeliskCore/VectorT.h
+++ Obelisk/ObeliskCore/VectorT.h
@@ -237,6 +237,11 @@ namespace obelisk
 		}
 	};
 
+	using Vector2f = Vector2<float>;
+	using Vector2d = Vector2<double>;
+	using Vector2i = Vector2<int>;
+	using Vector2u = Vector2<unsigned>;
+
 	using Vector3f = Vector3<float>;
 	using Vector3d = Vector3<double>;
 	using Vector3i = Vector3<int>;
