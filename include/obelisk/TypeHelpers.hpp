a2cdbfc3df5906244dbbf56d79042f1fad289709
jon-smith <jonsmith24601@outlook.com>
2020-10-31 13:38:19 +0000

    remove incorrect comment

--- include/obelisk/TypeHelpers.hpp
+++ include/obelisk/TypeHelpers.hpp
@@ -2,11 +2,10 @@
 
 namespace obelisk
 {
-	// Get element type of a container, retaining const-ness
-	template <typename T>
-	using ElementType = std::remove_reference_t<decltype(*std::begin(std::declval<T&>()))>;
+// Get element type of a container, retaining const-ness
+template <typename T>
+using ElementType = std::remove_reference_t<decltype(*std::begin(std::declval<T &>()))>;
 
-	// Get element type of a container, retaining const-ness
-	template <typename T>
-	using IteratorType = decltype(std::begin(std::declval<T&>()));
-}
+template <typename T>
+using IteratorType = decltype(std::begin(std::declval<T &>()));
+} // namespace obelisk
